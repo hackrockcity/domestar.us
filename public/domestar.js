1b5e38dd0cdb2da50fb3608167f5ae29de4c02db
Justin Day <justin@jplt.com>
2014-06-21 18:55:16 +0000

    Multi connection mode
    Trying to debug why Mobile Safari just stops sending events after awhile..

--- public/domestar.js
+++ public/domestar.js
@@ -1,7 +1,22 @@
 var state = { "id":genid() };
 var timer;
-var wsurl = "ws://ws.domestar.us:8000/phone";
-var ws = new WebSocket(wsurl);
+var wsLocalUrl = "ws://wsl.domestar.us:8000/phone";
+var wsRemoteUrl = "ws://ws.domestar.us:8000/phone";
+var wsl;
+var wsr;
+var lastTick;
+
+setup();
+
+function setup() {
+	if (window.DeviceOrientationEvent) {
+		window.addEventListener('deviceorientation', handleDeviceOrientation, true);
+	}
+
+	setupButtons();
+	wsl = setupWebsocket(wsLocalUrl);
+	wsr = setupWebsocket(wsRemoteUrl);
+}
 
 function genid() {
 	var wyattEpoch = new Date().getTime() - new Date("May 18, 2014 04:55").getTime()
@@ -10,15 +25,16 @@ function genid() {
     return wyattEpoch.toString(16) + "-" + rand.toString(16);
 }
 
-function setupWebsocket() {
-	console.log("Connecting to %s", wsurl);
-	ws = new WebSocket(wsurl);
-	ws.onopen = function(e) { console.log("Connected"); }
+function setupWebsocket(url, attempt) {
+	console.log("Connecting to %s", url);
+	var ws = new WebSocket(url);
+	ws.onopen = function(e) { attempt=0; console.log("Connected"); }
 	ws.onmessage = function(e) { console.log('message: %s', e.data); }
 	ws.onclose = function(e) { 
-		console.log("Connection closed." );
-		window.setTimeout(setupWebsocket, 1000);
+		console.log("Connection closed.  Reconnect");
+		window.setTimeout(function() { setupWebsocket(url, attempt||0+1) }, Math.min(attempt, 15) * 1000);
 	};
+	return ws;
 }
 
 function setupButtons() {
@@ -46,31 +62,32 @@ function handleDeviceOrientation(e) {
 }
 
 function handleDown(e) {
+	console.log("Down");
 	state['t'] = "start";
 	state['c'] = e.target.getAttribute('data-hue');
 	handleTick(e);
 	state['t'] = "down";
+
+	// TODO FIXME This breaks multitouch
 	window.clearInterval(timer);
 	timer = window.setInterval(handleTick, 100);
 }
 
 function handleUp(e) {
+	console.log("Up");
 	state['t'] = "end";
 	state['c'] = e.target.getAttribute('data-hue');
 	window.clearInterval(timer);
+	timer = null;
 	handleTick(e);
 }
 
 function handleTick(e) {
 	var msg = JSON.stringify(state);
 
-	ws.send(msg);
+	if (wsl.readyState == 1)
+		wsl.send(msg);
+	else if (wsr.readyState == 1)
+		wsr.send(msg);
 }
-
-if (window.DeviceOrientationEvent) {
-	window.addEventListener('deviceorientation', handleDeviceOrientation, true);
-}
-
-setupButtons();
-setupWebsocket();
 
